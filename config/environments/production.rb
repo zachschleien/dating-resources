0e53c181bf925d189de54f8d69ad3882f371dc42
zachschleien <zschleien@gmail.com>
2016-05-21 20:13:55 -0400

    pre compile assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Configure static file server for tests with Cache-Control for performance.
+  config.serve_static_files   = true
+  config.static_cache_control = 'public, max-age=3600'
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
